74d4b79aeb5c2f123ae286a3c16008c4c72a6dbd
Adalberto Perdomo Abreu <adalberto.devs@gmail.com>
2026-04-16 18:44:18 +0100

    feat: setup with reset.css, tokens, base and meta tags

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+import "./styles/reset.css";
+import "./styles/tokens/index.css";
+import "./styles/base.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
